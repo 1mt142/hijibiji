a6497578fbbd96f3429e0196ed9edd6ab52c1511
Mohsin Khandoker <mohsin.khondoker@upaybd.com>
2022-12-21 16:44:29 +0600

    hijibiji -1

--- blogapp/views.py
+++ blogapp/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import *
+from django.http import HttpResponse
+
 from django.db.models import F
 
 from .serializers import BlogPostSirializer
@@ -11,11 +13,27 @@ from .serializers import BlogPostSirializer
 class CreateBlog(APIView):
 
     def post(self, request, *args, **kwargs):
+        print("Request Body:", request.data)
+        print("Request args:", args)
+        print("Request kwargs:", kwargs)
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
 
     def get(self, request, *args, **kwargs):
-        post = BlogPost.objects.all().values()
-        print("post",list(post))
+        print("Request Body:", request.query_params)
+        print("All Func of request :", dir(request))
+        print("Request args:", args)
+        print("Request kwargs:", kwargs)
+        # For All
+        post = BlogPost.objects.all()
+        categoryname = post.annotate(categoryname=F('category__name'))
+        print("categoryname", categoryname)
+        # For specific fields like only for 'title','post_body'
+        post2 = BlogPost.objects.values_list('title', 'post_body')
+        print('Query::', post2.values().query)
+        print('Post Values::', post2)
+        print("DIR QUERY", dir(post2))
+        # print("post",list(post))
         # serializer = BlogPostSirializer(post, many=True)
         # print("serializer", serializer)
-        return Response({"data": list(post)}, status=status.HTTP_200_OK)
+        return Response({"data": ""}, status=status.HTTP_200_OK)
+        #return HttpResponse(BlogPost.objects.only('title'), content_type="application/json")
